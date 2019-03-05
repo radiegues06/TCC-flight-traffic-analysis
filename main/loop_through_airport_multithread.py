cdc46cdf5e4c69fbb585bbf9ff2a6160f70c009a
Rafael <rafaelamanciodiegues@gmail.com>
2019-03-05 08:52:27 -0300

    Error 402 solved

--- main/loop_through_airport_multithread.py
+++ main/loop_through_airport_multithread.py
@@ -7,20 +7,23 @@ Created on Tue Feb 26 14:49:33 2019
 Collect all arrivals and depart data for each airport, writing it all in a file
 """
 
-import requests
+import requests, json
 import threading
 import csv
 import math
 import time
 import calendar
+import os
+#from selenium import webdriver
 
 def resp_filter(siglum, page):
     resp = api_request()
     
     try:
-        json_parse = resp.json()
+        json_parse = json.loads(resp.text)
     except:
-        file_out.write('JSON reading error,' + str(resp.status_code) + ' , ' + resp.url + ' , ' +  siglum + ',' + str(page) + '\n')
+        file_out.write('JSON reading error,' + str(resp.status_code) + ' , ' + resp.url + ' , ' +  siglum + ',' + str(page) + 
+                       ',' + str(resp.content) + '\n')
         return
         
     json_node = json_parse['result']['response']['airport']['pluginData']['schedule'][parameters['plugin-setting[schedule][mode]']]['data']    
@@ -29,11 +32,16 @@ def resp_filter(siglum, page):
         flight = json_node[i]['flight']
         '''Filtering, excluing some data'''
         # Only the last day
-        '''current_time = time.gmtime()
+        
+        flight_timestamp = flight['time']['scheduled']['departure']
+        
+        current_time = time.gmtime()
         yesterday_timestamp = calendar.timegm((current_time.tm_year, current_time.tm_mon, current_time.tm_mday - 1, 
                                                0, 0, 0, current_time.tm_wday -1, current_time.tm_yday, current_time.tm_isdst))
         today_timestamp = calendar.timegm((current_time.tm_year, current_time.tm_mon, current_time.tm_mday, 
-                                               0, 0, 0, current_time.tm_wday, current_time.tm_yday, current_time.tm_isdst))'''
+                                               0, 0, 0, current_time.tm_wday, current_time.tm_yday, current_time.tm_isdst))
+        
+        if not( yesterday_timestamp < flight_timestamp and flight_timestamp < today_timestamp): continue
         
         # Printing flight if it has passed the filters
         
@@ -54,7 +62,7 @@ def airport_chunk(start,end):
             resp_filter(siglum, 0)
         
     else:
-        for k in range(0,-11, -1):
+        for k in range(0,-7, -1):
             parameters['page'] = k
             
             for j in ['arrivals']:#, 'departures']:
@@ -122,7 +130,13 @@ def api_request():
                         headers = headers, params = parameters)
     
     if resp.status_code != 200:
-        resp = requests.get(str(resp.url), headers = headers)
+        resp = requests.get(resp.url, headers = headers)
+        '''browser.get(resp.url)
+        try:
+            resp = browser.find_element_by_xpath("/html/body/div/div/div/div[2]/div/div/div[2]/pre")
+        except:
+            pass'''
+        
     
     return resp
     
@@ -134,19 +148,25 @@ if __name__ == '__main__':
     test_mode = 0
     
     # Importing the csv file to a matrix
-    file = open('Airports_list.csv', 'r')
+    scriptDirectory = os.path.dirname(__file__)
+    
+    file = open(os.path.join(scriptDirectory,'data/Airports_list.csv'), 'r')
     reader = csv.reader(file)
     data_list = list(reader)
     
+    # Firefor browser
+    #browser = webdriver.Firefox()
+    
     # Creating and setting new file where the dara will be written
-    file_out = open('flight_data.txt','w', encoding='utf-8')
+    file_out = open(os.path.join(scriptDirectory,'data/flight_data.txt'),'w', encoding='utf-8')
     format_out = '{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16}\n'
     file_out.write(format_out.format('Flight number', 'Status', 'Airplane Model', 'Airplane registration', 'Airplane owner', 'Airline',
                                      'Origin', 'Origin Terminal', 'Origin Gate', 'Destination', 'Destination Terminal', 'Destination Gate',
                                      'Scheduled departure', 'Scheduled arrival', 'Real departure', 'Real arrival', 'Duration'))
     
     # Setting internet parameters
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0"}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0',
+               'Cookie': '__cfduid=d52ce2146a6bf0758bd8688df953cb9fa1551100107; cookie_law_consent=1; _frc=OqXb3FuvDi5WJmBHxxrEo_SLCMEr-D_Y-6QmEX9b4E5e3YHKurUQp3OVRRHm0jmtL5EJAUanbr__7coMf-Uo5TGWFaahpLO4BHqL1ticNQyoJuxd2FwEA4CaPHUNxelZKr5G4AHJ-NBvjy8mPqX6dma-noWnZvHqTpd9PeSSZU1asyALfnNrelhnupyMalr3nSb-JYavyDWnzdy5aYs70S19AvqRXJReiKR1FF0iTzua8KJNFJk04T2nn1-REHXy; _frr=1; FR24ID=49hni7pbrp3ofmrt1bgv7t8ol5etcd2bfat8lhidn38ooph13983; _frpk=OqXb3FuvDi5WJmBHxxrEow; gpAuth=OqXb3FuvDi5WJmBHxxrEo43BODvpW4-HRQHfER3AidJQTPONfBOHSxNI-3-Q4cnj3W57cptnDzTS-q-mZE9GiVM1KCGHaINmwRyBkEfFJFjlNeicg55lRuZB9DQtinMJbhne6h8AgPowVDwtS6DnDNI9Hy7miBxfa6hScpLa-3-OwdAkGpzbmTS_6GF3U3xm2C53MvkTKUIA14GYgbFObk0lCFBY3qoxaSGjkvHehQhIXWyLSBSwjlSSw237XVAa0oHcxcBsvFFJngpBnqCGLmO9hsdZqRucA3KvKsjuyqgHhWv9yxB8MxvK4be2At4RWGV2pOXOaVSGjJfr02V6yDm5YT1zCvjIPhdiCvRfhr742VQBRee8mChw197LJo8Hzm0JXUSO470QHnkYtBV6kNB9gBFZRPEzy5GVeQoq9BWnDKWnqpIqxV9NqvNQ-Ab61XyVh36S0IV8a0lBKQuVZo4MWqe7lguRKEttWSEf6c3n_Y_JGsO49SrnXdUuHQXXbwGaoVbia0r6HkRTW_tB89zplcPqdHcXFD62TgbELPg; hasConsentedToTerms=yes; _frPl=OqXb3FuvDi5WJmBHxxrEo2VTLiHTwQ1p4wL6oifEDwo'}
     
     current_timestamp = calendar.timegm(time.gmtime())
     
